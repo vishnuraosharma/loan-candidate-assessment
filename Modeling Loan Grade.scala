3febc326e67dd82de50671a5a35360bade604504
Zehao Song <song.zeh@northeastern.edu>
2024-11-24 20:06:46 -0500

    Updated MultiLayer, more detailed training process with cross validation. Traing result improve by 3%. Working on improving over all distribution of data and checkpointing the othyer model

--- Modeling Loan Grade.scala
+++ Modeling Loan Grade.scala
@@ -28,7 +28,11 @@ import org.apache.spark.ml.feature.{StringIndexer, OneHotEncoder, VectorAssemble
 import org.apache.spark.ml.Pipeline
 
 // Index labels, adding metadata to the label column. Fit on whole dataset to include all labels in index.
-val labelIndexer = new StringIndexer().setInputCol("loan_grade").setOutputCol("indexedLabel").fit(rawData)
+val labelIndexer = new StringIndexer()
+  .setInputCol("loan_grade")
+  .setOutputCol("indexedLabel")
+  .setStringOrderType("alphabetAsc")  // This will order labels alphabetically
+  .fit(rawData)
 
 // Index and then OneHotEncode features
 val homeOwnershipIndexer = new StringIndexer().setInputCol("person_home_ownership").setOutputCol("homeOwnershipIndex").fit(rawData)
@@ -111,27 +115,75 @@ import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 // The first layer is the input layer (number of features),
 // the middle layers are the hidden layers
 // the last layer is the output layer (number of classes)
-val layers = Array[Int](19, 64, 32, 64, 7)  // Change 7 to the number of classes you have
+//val layers = Array[Int](19, 64, 32, 64, 7)  // Change 7 to the number of classes you have
+//
+//val mlp = new MultilayerPerceptronClassifier()
+//  .setLabelCol("indexedLabel")
+//  .setFeaturesCol("features")
+//  .setMaxIter(100)
+//  .setLayers(layers)
+//
+//val mlpModel = mlp.fit(trainingData)
+//val mlpPredictions = mlpModel.transform(validationData)
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.tuning.{ParamGridBuilder, CrossValidator}
 
+// Reduce the number of layers and neurons to simplify the network
+val layers = Array[Int](19, 15, 15, 7)  // Use fewer neurons
+
+// Initialize the MLP
 val mlp = new MultilayerPerceptronClassifier()
   .setLabelCol("indexedLabel")
   .setFeaturesCol("features")
-  .setMaxIter(100)
   .setLayers(layers)
+  .setBlockSize(128)
+  .setSeed(1234L)
+  .setSolver("l-bfgs")  // Use 'sgd' solver for faster convergence
+  .setTol(1E-6)
+  .setStepSize(0.03)
 
-val mlpModel = mlp.fit(trainingData)
-val mlpPredictions = mlpModel.transform(validationData)
+// Define the parameter grid with fewer combinations
+val paramGrid = new ParamGridBuilder()
+  .addGrid(mlp.maxIter, Array(50, 100))
+  .addGrid(mlp.layers, Array(Array(19, 15, 15, 7)))  // Only one configuration to try
+  .build()
 
-
-// Evaluation
-// Evaluate accuracy
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+// Evaluator
 val evaluator = new MulticlassClassificationEvaluator()
   .setLabelCol("indexedLabel")
   .setPredictionCol("prediction")
   .setMetricName("accuracy")
 
+// Cross Validator
+val cv = new CrossValidator()
+  .setEstimator(mlp)
+  .setEvaluator(evaluator)
+  .setEstimatorParamMaps(paramGrid)
+  .setNumFolds(3)  // Use 3 folds for faster results
+  .setParallelism(1)  // Keep parallelism low for a single machine
+
+// Run cross-validation
+val cvModel = cv.fit(trainingData)
+
+// Use the test data to measure the accuracy
+val mlpPredictions = cvModel.transform(validationData)
+
+// Evaluate the best model
 val accuracy = evaluator.evaluate(mlpPredictions)
 println(s"Test Accuracy = $accuracy")
 
-val testPredictions = model.transform(finalTestOutput)
+
+
+// Evaluation
+// Evaluate accuracy
+//import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+//val evaluator = new MulticlassClassificationEvaluator()
+//  .setLabelCol("indexedLabel")
+//  .setPredictionCol("prediction")
+//  .setMetricName("accuracy")
+//
+//val accuracy = evaluator.evaluate(mlpPredictions)
+//println(s"Test Accuracy = $accuracy")
+//
+//val testPredictions = model.transform(finalTestOutput)
